c32733d31fd499b986b02b894221954f4461e229
Seonghee <heeracle@naver.com>
2022-03-30 18:18:53 +0900

    페이징

--- src/main/java/com/seonghee/myhome/controller/BoardController.java
+++ src/main/java/com/seonghee/myhome/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.seonghee.myhome.model.Board;
 import com.seonghee.myhome.repository.BoardRepository;
 import com.seonghee.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,8 +28,14 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model){
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required=false, defaultValue = "") String searchText){     //parameter로 page설정하기
+        //Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board>boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);  //최솟값 1
+        int endPage = Math.min(boards.getPageable().getPageNumber() + 4, boards.getTotalPages());
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards); //model에 담긴 데이터를 thymeleaf에서 사용할 수 있게 된다.
         //boards라는 key값에 boards라는 데이터를 준다.
         return "board/list";
--- src/main/java/com/seonghee/myhome/repository/BoardRepository.java
+++ src/main/java/com/seonghee/myhome/repository/BoardRepository.java
@@ -1,8 +1,11 @@
 package com.seonghee.myhome.repository;
 
 import com.seonghee.myhome.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -10,4 +13,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     List<Board> findByTitle(String title);
     List<Board> findByTitleOrContent(String title, String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
