37c617bcafac063b69921a206817f27c428036a5
Seonghee <heeracle@naver.com>
2022-03-30 15:09:10 +0900

    RestfulAPI CRUD

--- src/main/java/com/seonghee/myhome/repository/BoardRepository.java
+++ src/main/java/com/seonghee/myhome/repository/BoardRepository.java
@@ -4,6 +4,10 @@ import com.seonghee.myhome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
